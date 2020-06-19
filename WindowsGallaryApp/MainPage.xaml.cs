bc423a4184bf2f41e5483335473482abd6b836c1
Gabriel Vazquez <t-gavazq@microsoft.com>
2020-06-19 13:33:46 -0700

    Created Gallery with GridView and buttons for pager

--- WindowsGallaryApp/MainPage.xaml.cs
+++ WindowsGallaryApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WindowsGallaryApp.Scripts;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -21,21 +23,58 @@ namespace WindowsGallaryApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class MainPage : Page
     {
+        private List<CustomImage> imageList;
+        private int pageIndex;
+        private int imagesPerPage = 12;
+
         public MainPage()
         {
             this.InitializeComponent();
+            imageList = CustomImage.GetImages(120);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            if (sender is Button b)
-            {
-                string name = b.Name;
+            base.OnNavigatedTo(e);
+            pageIndex = 0;
+            UpdateGrid();
+        }
 
-                b.Content = "Clicked";
+        private void UpdateGrid()
+        {
+            ImagesCVS.Source = new ObservableCollection<CustomImage>(GetPageImages());
+        }
+
+        private List<CustomImage> GetPageImages()
+        {
+            return imageList.GetRange(pageIndex * imagesPerPage, imagesPerPage);
+        }
+
+        private void PagerBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            pageIndex = Math.Clamp(--pageIndex, 0, 9);
+            if (pageIndex == 0)
+            {
+                PagerBackButton.IsEnabled = false;
             }
+
+            PagerForwardButton.IsEnabled = true;
+
+            UpdateGrid();
+        }
+
+        private void PagerForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            pageIndex = Math.Clamp(++pageIndex, 0, 9);
+            if (pageIndex == 9)
+            {
+                PagerForwardButton.IsEnabled = false;
+            }
+            PagerBackButton.IsEnabled = true;
+            UpdateGrid();
         }
     }
 }
---
+++ WindowsGallaryApp/Scripts/CustomImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsGallaryApp.Scripts
+{
+    class CustomImage
+    {
+        private static Random rand = new Random();
+        private static List<string> titles = new List<string>() { "Cliffs", "Grapes", "Landscape1", "Landscape2", "Landscape3", "Landscape4", "Landscape5", "Landscape6", "Landscape7", "Landscape8", "Rainier", "Sunset", "Treetops", "Valley" };
+        private static List<string> filenames = new List<string>() { "cliff.jpg", "grapes.jpg", "LandscapeImage1.jpg", "LandscapeImage2.jpg", "LandscapeImage3.jpg", "LandscapeImage4.jpg", "LandscapeImage5.jpg", "LandscapeImage6.jpg", "LandscapeImage7.jpg", "LandscapeImage8.jpg", "rainier.jpg", "sunset.jpg", "treetops.jpg", "valley.jpg" };
+        
+        public string Title { get; set; }
+        public string ImageLocation { get; set; }
+        public string Description { get; set; }
+
+        public CustomImage(string Title, string ImageLocation, string Description = "")
+        {
+            this.Title = Title;
+            this.ImageLocation = ImageLocation;
+            this.Description = Description;
+        }
+
+        private static CustomImage GenerateImage()
+        {
+            int index = rand.Next(0, filenames.Count);
+            return new CustomImage(titles[index], @"Assets/Media/" + filenames[index]);
+        }
+        public static List<CustomImage> GetImages(int numImages)
+        {
+            List<CustomImage> images = new List<CustomImage>();
+
+            for (int i = 0; i < numImages; i++)
+            {
+                images.Add(GenerateImage());
+            }
+            return images;
+        }
+    }
+}
